76d0b7824b192a8a4829cdb62ccd62a8032cd9be
Diploidal <diploidal@gmail.com>
2020-11-15 21:05:39 +0100

    Add fetch data mechanism to github api

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="search-container d-grid">
     <h1 class="mb-5">{{ instruction }}</h1>
-    <form>
-      <input 
+    <form @submit.prevent="handleRequest(name)">
+      <input
+        v-model="name"
         type="text" 
         className="form-control" 
         placeholder="Github Username" 
         aria-label="Username" 
         aria-describedby="basic-addon1"
-        value=""
       />
       <div class="d-grid">
+        <code class="mt-2">{{name}}</code>
         <button type="submit" class="btn btn-primary mt-4">
           Search
         </button>
@@ -21,11 +22,40 @@
 
 <script>
 export default {
-  name: 'Header',
   props: {
     instruction: String
+  },
+  data () {
+    return {
+      name: '',
+      userInformation: {},
+      userRepositories: {}
+    }
+  },
+  methods: {
+    async handleRequest(name) {
+      const API_LINK = `https://api.github.com/users`;
+      let requestData = {};
+      const response = await fetch(`${API_LINK}/${name}`)
+          if(!response.ok) {
+      requestData.response = response
+      console.log(requestData)
+    } else {
+      response.json().then((data) => {
+        requestData.user = data;
+        requestData.response = response;
+      })
+      const repoResponse = await fetch(`${API_LINK}/${name}/repos`);
+      repoResponse.json().then((data) => {
+        requestData.repos = data;
+        requestData.response = response;
+        console.log(requestData);
+      })
+    }
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
