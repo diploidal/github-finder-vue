57f9f008817a982f58cdd6275c35c4de1148bc5e
Diploidal <diploidal@gmail.com>
2020-11-15 19:01:15 +0100

    Base project cleanup, add header component, add boostrap v5 alpha css

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search-container d-grid">
+    <h1 class="mb-5">{{ instruction }}</h1>
+    <form>
+      <input 
+        type="text" 
+        className="form-control" 
+        placeholder="Github Username" 
+        aria-label="Username" 
+        aria-describedby="basic-addon1"
+        value=""
+      />
+      <div class="d-grid">
+        <button type="submit" class="btn btn-primary mt-4">
+          Search
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    instruction: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.search-container {
+  padding: 6rem 0px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+form {
+  justify-self: center;
+}
+</style>
